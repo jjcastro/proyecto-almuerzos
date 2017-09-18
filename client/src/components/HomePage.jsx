6707a85909db23e74d44ffe6ac9e9b2f0a8d3a9b
Andrea Paredes <aj.paredes10@uniandes.edu.co>
2017-09-18 00:52:55 -0500

    home

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Card, CardTitle } from 'material-ui/Card';
+import { Card } from 'material-ui/Card';
 
 
 const HomePage = () => (
   <Card className="container">
-    <CardTitle title="Comparte Tu Mesa" subtitle="Bienvenido" />
+      <h1>Comparte Tu Mesa</h1>
+      <img src="https://cdn0.iconfinder.com/data/icons/hotel-and-restaurant-cool-vector-3/128/136-512.png" alt="Comparte tu mesa icon"/>
       <h4>Nosotros te ayudaremos a encontrar un compañero o compañera con quien puedes pasar tu hora del almuerzo.</h4>
   </Card>
 );
